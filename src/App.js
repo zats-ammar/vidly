3fa78af94fe3ce19c29531f0f96d8d730efaeded
Ammar <ammar@inexisconsulting.com>
2020-05-25 12:09:04 +0530

    user registration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
+//importing objects from 3rd party libraries
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+//importing components of our application
 import Movies from "./components/movies.jsx";
 import NavBar from "./components/navBar";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/common/notfound";
 import MovieForm from "./components/movieForm";
-import LoginForm from './components/loginForm';
-import RegisterForm from './components/registerForm';
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout.jsx";
+import ProtectedRoute from "./components/common/protectedRoute.jsx";
+//importing services of our application
+import auth from "./services/authService";
+//importing css modules
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <React.Fragment>
-        <NavBar />
+        <ToastContainer />
+        <NavBar user={user} />
         <main className="container">
-          <Switch> 
+          <Switch>
             {/* routes are work in an order */}
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
-            <Route path="/movies/new" component={MovieForm} />
-            <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies} />
+            <Route path="/logout" component={Logout} />
+            {/* protecting a route */}
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            {/* we cannot directly pass additional props to the child component(Movies) which is under Route component as like in Navbar(user={user}) */}
+            {/* in order to pass, we need to use the render attribute */}
+            {/* it takes a function 'props', and returns movies component */}
+            {/* it is essential to add all these props({...props}) to the child component, because it includes history,match,etc that react will automatically inject while routing */}
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={user} />}
+            />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
+//'* as userService' syntax -> import all the exported functions from the path and set to userService object
+import * as userService from "../services/userService"; 
+import auth from "../services/authService";
 
 class RegisterForm extends Form {
   state = {
@@ -22,8 +25,25 @@ class RegisterForm extends Form {
       .label("Name")
   };
 
-  doSubmit = () => {
-    console.log("Register form submitted");
+  doSubmit = async () => {
+    try{
+      //convention when defining custom headers->  start with x-....
+      //in order to read custom http headers from client, you need to set "access-control-expose-headers" standard http header in backend
+      const response = await userService.register(this.state.data);
+      auth.loginWithJwt(response.headers['x-auth-token']);
+      //this.props.history.push("/"); history.push won't invoke componentDidMount() of our app component
+      window.location = '/';
+    }
+    catch (ex){
+      //usually a register endpoint should return 2 type of response
+      //200 for success
+      //400(bad request) if a user has already registered
+      if(ex.response && ex.response.status === 400){
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
